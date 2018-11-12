e3483091bacadb7b36d8203afa41a27df61743cc
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-12 23:17:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(num)
   if num < 0 or num == 0 or num == 1
     return false
   else
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
+    (2..num-1).to_a.all? do |factors|
+      num % factors != 0
     end
   end
 end
